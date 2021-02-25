be0603d96d0b525748f602a7b4e22b3be1bfa74a
mathewchamberlain <mathew.chamberlain@sanofi.com>
2021-02-25 16:30:02 -0500

    Added vignette for SingleR vs. Signac comparison

--- vignettes/signac-Seurat_AMP_RA.R
+++ vignettes/signac-Seurat_AMP_RA.R
@@ -18,11 +18,10 @@ knitr::opts_chunk$set(
   warning = FALSE,
   time_it = TRUE
 )
-#celltypes_fast = readRDS("./fls/celltypes_fast_citeseq.rds")
-#celltypes = readRDS("./fls/celltypes_citeseq.rds")
-# pbmc = readRDS("fls/pbmcs_signac_citeseq.rds")
 celltypes = readRDS(file = "fls/celltypes_amp_synovium.rds")
-celltypes_fast = readRDS(file = "fls/celltypes_fast_synovium_celltypes.rds")
+True_labels = readRDS(file = "fls/celltypes_amp_synovium_true.rds")
+load(file = "fls/SingleR_Results.rda")
+
 
 ## ----read CELSeq2, message = F, eval = F--------------------------------------
 #  ReadCelseq <- function (counts.file, meta.file)
@@ -51,6 +50,14 @@ celltypes_fast = readRDS(file = "fls/celltypes_fast_synovium_celltypes.rds")
 #  MitoFrac = Matrix::colSums(E[logik,]) / Matrix::colSums(E) * 100
 #  E = E[,MitoFrac < 20]
 
+## ----filter celseq, message = F, eval = F-------------------------------------
+#  require(SingleR)
+#  data("hpca")
+#  Q = SingleR(sc_data = E, ref_data = hpca$data, types = hpca$main_types, fine.tune = F, numCores = 16)
+#  save(file = "fls/SingleR_Results.rda", Q)
+#  True_labels = M$type[match(colnames(E), M$cell_name)]
+#  saveRDS(True_labels, file = "fls/celltypes_amp_synovium_true.rds")
+
 ## ----setupSeurat, message = F, eval = F---------------------------------------
 #  library(Seurat)
 
@@ -59,7 +66,7 @@ celltypes_fast = readRDS(file = "fls/celltypes_fast_synovium_celltypes.rds")
 #  synovium <- CreateSeuratObject(counts = E, project = "FACs")
 #  
 #  # run sctransform
-#  synovium <- SCTransform(synovium)
+#  synovium <- SCTransform(synovium, verbose = F)
 
 ## ----Seurat 2, message = T, eval = F------------------------------------------
 #  # These are now standard steps in the Seurat workflow for visualization and clustering
@@ -71,27 +78,55 @@ celltypes_fast = readRDS(file = "fls/celltypes_fast_synovium_celltypes.rds")
 #  library(SignacX)
 
 ## ----Signac, message = T, eval = F--------------------------------------------
+#  # Run Signac
 #  labels <- Signac(synovium, num.cores = 4)
 #  celltypes = GenerateLabels(labels, E = synovium)
 
-## ----SignacFast, message = T, eval = F----------------------------------------
-#  # Run SignacFast
-#  labels_fast <- SignacFast(synovium, num.cores = 4)
-#  celltypes_fast = GenerateLabels(labels_fast, E = synovium)
+## ----Seurat Visualization 1110, message = F, echo = F-------------------------
+xx = celltypes$CellTypes
+xx = as.character(xx)
+xx[xx == "Plasma.cells"] = "B"
+xx[xx == "NonImmune"] = as.character(celltypes$CellStates)[xx == "NonImmune"]
+xx[xx == "B"] = "B"
+xx[xx == "Fibroblasts"] = "F"
+xx[xx == "MPh"] = "MPh"
+xx[xx == "TNK"] = "T"
+xx[celltypes$CellStates == "NK"] = "NK"
+True_labels[True_labels == "B cell"] = "B"
+True_labels[True_labels == "Fibroblast"] = "F"
+True_labels[True_labels == "Monocyte"] = "MPh"
+True_labels[True_labels == "T cell"] = "T"
+tab = table(FACs = True_labels, Signac = xx)
+vcd::assoc(tab, shade=T, legend=T)
 
-## ----Compare 1, echo = F, eval = T--------------------------------------------
-knitr::kable(table(Signac = celltypes$CellTypes, SignacFast = celltypes_fast$CellTypes), format = "html")
+## ----Seurat Visualization 11110, message = F, echo = T------------------------
+logik = xx != "Unclassified"
+Signac_Accuracy = round(sum(xx[logik] == True_labels[logik]) / sum(logik) * 100, 2)
+Signac_Accuracy
 
-## ----Compare 2, echo = F, eval = T--------------------------------------------
-knitr::kable(table(Signac = celltypes$CellStates, SignacFast = celltypes_fast$CellStates), format = "html")
+## ----Seurat Visualization 10, message = F, echo = F---------------------------
+xx = Q$labels
+xx[xx %in% c("Macrophage", "DC", "Monocyte", "Platelets", "Neutrophils")] = "MPh"
+xx[xx %in% c("B_cell", "Pre-B_cell_CD34-", "Pro-B_cell_CD34+")] = "B"
+xx[xx == "Fibroblasts"] = "F"
+xx[xx == "T_cells"] = "T"
+xx[xx %in% c("Astrocyte", "Osteoblasts", "Tissue_stem_cells", "Smooth_muscle_cells", "MSC")] = "NonImmune"
+xx[xx == "NK_cell"] = "NK"
+xx[xx == "Chondrocytes"] = "Chondr."
+tab = table(FACs = True_labels, SingleR = xx)
+vcd::assoc(tab, shade=T, legend=T)
+
+## ----Seurat Visualization 111110, message = F, echo = T-----------------------
+logik = xx != "Unclassified"
+SingleR_Accuracy = round(sum(xx[logik] == True_labels[logik]) / sum(logik) * 100, 2)
+SingleR_Accuracy
 
 ## ----save results, message = F, eval = F--------------------------------------
-#  saveRDS(synovium, file = "fls/seurat_obj_amp_synovium.rds")
-#  saveRDS(celltypes, file = "fls/celltypes_amp_synovium.rds")
-#  saveRDS(celltypes_fast, file = "fls/celltypes_fast_amp_synovium_celltypes.rds")
+#  saveRDS(synovium, file = "synovium_signac.rds")
+#  saveRDS(celltypes, file = "synovium_signac_celltypes.rds")
 
 ## ----save.times, include = FALSE, eval = F------------------------------------
-#  write.csv(x = t(as.data.frame(all_times)), file = "fls/tutorial_times_SignacFast_AMP.csv")
+#  write.csv(x = t(as.data.frame(all_times)), file = "fls/tutorial_times_signac-seurat_singler_AMP_RA.csv")
 
 ## ---- echo=FALSE--------------------------------------------------------------
 sessionInfo()
